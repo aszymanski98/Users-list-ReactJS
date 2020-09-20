4b6c8af86a5fba2f9aee06f57d8f59a839680fad
aszymanski98 <adam.szymanski@protonmail.com>
2020-09-20 17:22:23 +0200

    Updates

---
+++ precache-manifest.8f67d8d8390183773ae6bb437dd5eb6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6732ebc826588f5bc52c3e96ee064c18",
+    "url": "/Users-list-ReactJS/index.html"
+  },
+  {
+    "revision": "b36464db8f9f5a0aa4f3",
+    "url": "/Users-list-ReactJS/static/css/main.9a65aba2.chunk.css"
+  },
+  {
+    "revision": "c7751375ebe5e689a254",
+    "url": "/Users-list-ReactJS/static/js/2.c1c804b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Users-list-ReactJS/static/js/2.c1c804b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36464db8f9f5a0aa4f3",
+    "url": "/Users-list-ReactJS/static/js/main.5a67847f.chunk.js"
+  },
+  {
+    "revision": "e2898942ce58cf39e4ec",
+    "url": "/Users-list-ReactJS/static/js/runtime-main.eb50d311.js"
+  }
+]);
